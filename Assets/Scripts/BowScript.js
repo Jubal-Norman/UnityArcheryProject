1158d7dcbce9dac6eaa84fb16acd5b159590f2e4
Jubal Norman <Jubal.norman@gmail.com>
2013-03-10 17:07:29 -0700

    Added ammo limit and HUD
    Added a limitation to ammo and a rectangle in the HUD to display it.

--- Assets/Scripts/BowScript.js
+++ Assets/Scripts/BowScript.js
@@ -3,6 +3,8 @@
 var arrow : Rigidbody;
 var speed = 50.0;
 var nocked = false;
+var ammoCountRect : Rect = Rect(25,25,50,25);
+var ammoCount : int = 10;
 
 function Start () {
 
@@ -19,17 +21,25 @@ function FireArrow () {
 	arrowClone.velocity = transform.TransformDirection(Vector3.forward * speed);
 	arrowClone.GetComponent(ArrowScript).Fired();
 	nocked = false;
+	ammoCount = ammoCount - 1;
+}
+
+function OnGUI()
+{
+	GUI.Label(ammoCountRect, ammoCount.ToString(), "box");
 }
 
 function Update () {
-	if(nocked == true) {
-		arrowClone.position = transform.position;
-		arrowClone.rotation = transform.rotation;
-	}
-	if(Input.GetButtonDown("Fire1")) {
-		NockArrow();
-	}
-	if(Input.GetButtonUp("Fire1")) {
-		FireArrow();
+	if(ammoCount > 0){
+		if(nocked == true) {
+			arrowClone.position = transform.position;
+			arrowClone.rotation = transform.rotation;
+		}
+		if(Input.GetButtonDown("Fire1")) {
+			NockArrow();
+		}
+		if(Input.GetButtonUp("Fire1")) {
+			FireArrow();
+		}
 	}
 }
